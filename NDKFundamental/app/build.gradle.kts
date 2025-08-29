442a0bca22bc8f8a184eb7e812ea141daef1f895
mdkhalekuzzaman <md.khalekuzzaman@devsstream.com>
2025-08-30 00:23:37 +0600

    NDK fundamental project added

--- NDKFundamental/app/build.gradle.kts
+++ NDKFundamental/app/build.gradle.kts
@@ -13,17 +13,11 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
     compileOptions {
